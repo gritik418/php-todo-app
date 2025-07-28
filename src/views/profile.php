9e9a4e66c2547d9309272d9bd640564e06b7342f
Ritik <160255700+gritik418@users.noreply.github.com>
2025-07-28 17:15:52 +0530

    feat: add profile page

--- src/views/profile.php
+++ src/views/profile.php
@@ -1,3 +1,26 @@
+<?php
+require_once __DIR__ . "/../config/database.php";
+
+session_start();
+
+$userId =  $_SESSION['user_id'];
+
+if ($userId) {
+    $statement = $conn->prepare("SELECT * FROM users where id = ?");
+    $statement->bindParam(1, $userId);
+    $statement->execute();
+
+    $user = $statement->fetch();
+    if (!$user['id']) {
+        header("Location: /login");
+    }
+} else {
+    header("Location: /login");
+}
+
+
+?>
+
 <div class="min-h-screen flex items-center justify-center px-4">
     <div class=" max-w-md w-full bg-white rounded-xl p-6 shadow-[0_4px_20px_rgba(9,81,139,0.1)]">
         <!-- Profile Image -->
@@ -6,36 +29,21 @@
                 src="/images/default-profile.png"
                 alt="Profile Picture"
                 class="w-24 h-24 rounded-full object-cover mb-4" />
-            <h2 class="text-xl font-semibold text-gray-800">Ritik Gupta</h2>
-            <p class="text-sm text-gray-500">ritik@example.com</p>
+            <h2 class="text-xl font-semibold text-gray-800"><?php echo $user['name']; ?></h2>
+            <p class="text-sm text-gray-500"><?php echo $user['email']; ?></p>
         </div>
 
         <!-- Profile Info -->
         <div class="space-y-4 text-gray-700">
             <div class="flex justify-between">
                 <span class="font-medium">Username:</span>
-                <span>@ritikgupta</span>
-            </div>
-            <div class="flex justify-between">
-                <span class="font-medium">Total Todos:</span>
-                <span>10</span>
-            </div>
-            <div class="flex justify-between">
-                <span class="font-medium">Location:</span>
-                <span>Delhi, India</span>
+                <span>@<?php echo $user['username']; ?></span>
             </div>
             <div class="flex justify-between">
                 <span class="font-medium">Joined:</span>
-                <span>July 2025</span>
+                <span><?php echo date('l, j F Y', strtotime($user['created_at'])) ?></span>
             </div>
         </div>
 
-        <!-- Action Button -->
-        <div class="mt-6 text-center">
-            <button
-                class="px-6 py-2 bg-[#bc2a77] hover:bg-[#d54390] text-white rounded-lg transition">
-                Edit Profile
-            </button>
-        </div>
     </div>
 </div>
